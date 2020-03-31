f50d95102bc6df764a073c6028cbb5b02709bcc4
Avner Maiberg <avnermaiberg@gmail.com>
2020-03-31 17:59:35 -0400

    [WIP] some improvements with parsing the token_id from sign-up and making API calls

--- src/request_helper.js
+++ src/request_helper.js
@@ -1,5 +1,5 @@
+import env from './env.json'
 var request = require("request");
-var env = require("dotenv-json")();
 
 export function createCustomer(paymentMethod) {
   return request(
@@ -8,7 +8,7 @@ export function createCustomer(paymentMethod) {
       url: process.env.SERVER + "/billing/create-customer/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: process.env.ACCESS_ID
+        Authorization: env.ACCESS_ID
       },
       body: paymentMethod
     },
@@ -27,7 +27,7 @@ export function designSubscription(requirements) {
       url: process.env.SERVER + "/billing/design-subscription/",
       headers: {
         "Content-Type": "application/json",
-        Authorization: process.env.ACCESS_ID
+        Authorization: env.ACCESS_ID
       },
       body: requirements
     },
@@ -38,3 +38,89 @@ export function designSubscription(requirements) {
     }
   );
 }
+
+  export function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+  
+
+function getAllUrlParams(url) {
+
+  // get query string from url (optional) or window
+  var queryString = url ? url.split('?')[1] : window.location.hash.slice(1);
+
+  // we'll store the parameters here
+  var obj = {};
+
+  console.log(queryString);
+
+  // if query string exists
+  if (queryString) {
+
+    // stuff after # is not part of query string, so get rid of it
+    //queryString = queryString.split('#')[1];
+
+    // split our query string into its component parts
+    var arr = queryString.split('&');
+
+    for (var i = 0; i < arr.length; i++) {
+      // separate the keys and the values
+      var a = arr[i].split('=');
+
+      // set parameter name and value (use 'true' if empty)
+      var paramName = a[0];
+      var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+      // (optional) keep case consistent
+      paramName = paramName.toLowerCase();
+      if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+      // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+      if (paramName.match(/\[(\d+)?\]$/)) {
+
+        // create key if it doesn't exist
+        var key = paramName.replace(/\[(\d+)?\]/, '');
+        if (!obj[key]) obj[key] = [];
+
+        // if it's an indexed array e.g. colors[2]
+        if (paramName.match(/\[\d+\]$/)) {
+          // get the index value and add the entry at the appropriate position
+          var index = /\[(\d+)\]/.exec(paramName)[1];
+          obj[key][index] = paramValue;
+        } else {
+          // otherwise add the value to the end of the array
+          obj[key].push(paramValue);
+        }
+      } else {
+        // we're dealing with a string
+        if (!obj[paramName]) {
+          // if it doesn't exist, create property
+          obj[paramName] = paramValue;
+        } else if (obj[paramName] && typeof obj[paramName] === 'string'){
+          // if property does exist and it's a string, convert it to an array
+          obj[paramName] = [obj[paramName]];
+          obj[paramName].push(paramValue);
+        } else {
+          // otherwise add the property
+          obj[paramName].push(paramValue);
+        }
+      }
+    }
+  }
+
+  return obj;
+}
+
+export default getAllUrlParams;
